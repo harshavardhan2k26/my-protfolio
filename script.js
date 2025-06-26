5ddba3b52e43acdd104448af1bbef33a1e223128
Your Name <vhvredy7@gmail.com>
2025-06-26 20:22:54 +0530

    fun projects

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
+// Toggle Password
 function togglePassword() {
-    const passwordField = document.getElementById("password");
-    const toggleButton = document.getElementById("togglePassword");
-
-    if (passwordField.type === "password") {
-        passwordField.type = "text";
-        toggleButton.textContent = "Hide Password";
-    } else {
-        passwordField.type = "password";
-        toggleButton.textContent = "Show Password";
-    }
+  const input = document.getElementById("password");
+  input.type = input.type === "password" ? "text" : "password";
 }
 
-
+// Toggle Dark Mode
 function toggleDark() {
-  document.getElementById("body").classList.toggle("dark-mode");
+  document.body.classList.toggle("dark-mode");
 }
 
-
+// Image Slider
 let count = 1;
 function nextImage() {
   count++;
   document.getElementById("slider").src = `https://picsum.photos/300?random=${count}`;
 }
-
